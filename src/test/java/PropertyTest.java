aa001807290f9980a8be6ceb576b8206ddc4dea0
dmgilmour <dmgilmour@pitt.edu>
2017-07-15 00:52:37 -0400

    More mortgaging tests and comments

--- src/test/java/PropertyTest.java
+++ src/test/java/PropertyTest.java
@@ -12,28 +12,33 @@ public class PropertyTest {
 
 	public void setup() {
 		p1 = new Player(0, 100, "dummy");
-		p2 = new Player(0, 50, "dummy");
+		p2 = new Player(1, 50, "dummy");
 		prop = new Property("nowhere", 50, 20);
+		prop.setOwner(p2);
+		prop.unmortgage();
 	}
 
+	// Test that mortgaging a property updates the "mortgaged" value
 	@Test
-	public void testMortgageStandard() {
+	public void mortgageStandardValidate() {
 		setup();
 		prop.setOwner(p1);
 		prop.mortgage();
 		assertTrue(prop.getMortgaged());
 	}
 
+	// Test that a valid mortgage will pay the owner the proper amount
 	@Test
-	public void testMortgagePayStandard() {
+	public void mortgageStandardPay() {
 		setup();
 		prop.setOwner(p1);
 		prop.mortgage();
 		assertEquals(p1.getMoney(), 125);
 	}
 
+	// Test that unmortgaging a property updates the "mortgaged" value
 	@Test
-	public void testUnmortgageStandard() {
+	public void unmortgageStandardValidate() {
 		setup();
 		prop.setOwner(p1);
 		prop.mortgage();
@@ -41,12 +46,78 @@ public class PropertyTest {
 		assertFalse(prop.getMortgaged());
 	}
 
+	// Test that a valid unmortgage will charge the owner the proper amount
 	@Test
-	public void testUnmortgagePayStandard() {
+	public void unmortgageStandardCharge() {
 		setup();
 		prop.setOwner(p1);
 		prop.mortgage();
 		prop.unmortgage();
 		assertEquals(p1.getMoney(), 75);
+	}
+
+	// Test that attempting to mortgage an already mortgaged property does 
+	// nothing
+	@Test
+	public void mortgageRedundantPay() {
+		setup();
+		prop.setOwner(p1);
+		prop.setMortgaged(true);
+		prop.mortgage();
+		assertEquals(p1.getMoney(), 100);
+	}
+
+	// Test that attempting to unmortgage an already unmortgaged property does
+	// nothing
+	@Test
+	public void unmortgageRedundantCharge() {
+		setup();
+		prop.setOwner(p1);
+		prop.setMortgaged(false);
+		prop.unmortgage();
+		assertEquals(p1.getMoney(), 100);
+	}
+
+	// Tests that one can unmortgage a property with exactly the price requested
+	@Test
+	public void unmortgageMoneyEqualsPriceValidate() {
+		setup();
+		prop.setOwner(p2);
+		prop.setMortgaged(true);
+		prop.unmortgage();
+		assertFalse(prop.getMortgaged());
+	}
+
+	// Tests that a player is properly charged for unmortgaging a property with
+	// price equal to their remaining money
+	@Test
+	public void unmortgageMoneyEqualsPriceCharge() {
+		setup();
+		prop.setOwner(p2);
+		prop.setMortgaged(true);
+		prop.unmortgage();
+		assertEquals(p2.getMoney(), 0);
+	}
+
+	// Tests that if a player cannot afford to unmortgage a property it will
+	// stay mortgaged
+	@Test
+	public void unmortgageTooExpensiveValidate() {
+		setup();
+		prop.setOwner(p2);
+		prop.setMortgaged(true);
+		prop.unmortgage();
+		assertTrue(prop.getMortgaged());
+	}
+
+	// Tests that if a player cannot afford to unmortgage a property they will
+	// not be charged
+	@Test
+	public void unmortgageTooExpensiveCharge() {
+		setup();
+		prop.setOwner(p2);
+		prop.setMortgaged(true);
+		prop.unmortgage();
+		assertEquals(p2.getMoney(), 50);
 	}
 }
