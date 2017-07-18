01005397660a9ab7c15ea052766b77c9b07d7234
dmgilmour <dmgilmour@pitt.edu>
2017-07-18 14:39:08 -0400

    Didn't commit it all apparently

---
+++ src/test/java/GameTest.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import game.Player;
+import game.Property;
+
+public class GameTest {
+
+	/*
+	Player p1;
+	Player p2;
+	Player p3;
+
+	Property prop1;
+	Property prop2;
+	Property prop3;
+
+	public void setup() {
+		prop1 = new Property("A", 1000, 500);
+		prop2 = new Property("B", 500, 250);
+		prop3 = new Property("C", 250, 125);
+		prop4 = new Property("D", 125, 75);
+		prop5 = new Property("E", 75, 50);
+		prop6 = new Property("F", 50, 25);
+
+		p1 = new Player(0, 2000, "P1");
+		p2 = new Player(1, 1500, "P2");
+		p3 = new Player(2, 1000, "P3");
+		
+		p1.addProperty(prop1);
+		p1.addProeprty(prop2);
+		p1.addProperty(prop3);
+		p2.addProperty(prop4);
+		p2.addProperty(prop5);
+	}
+	*/
+}
